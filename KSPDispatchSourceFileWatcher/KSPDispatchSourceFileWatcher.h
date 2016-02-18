c97f33f6eda1bbc2b2ac61e59c55ba5917cf17a3
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2016-02-18 11:17:56 +0300

    Initial Commit.

---
+++ KSPDispatchSourceFileWatcher/KSPDispatchSourceFileWatcher.h
@@ -0,0 +1,19 @@
+//
+//  KSPDispatchSourceFileWatcher.h
+//  KSPDispatchSourceFileWatcher
+//
+//  Created by Konstantin Pavlikhin on 18.02.16.
+//  Copyright © 2016 Konstantin Pavlikhin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KSPDispatchSourceFileWatcher.
+FOUNDATION_EXPORT double KSPDispatchSourceFileWatcherVersionNumber;
+
+//! Project version string for KSPDispatchSourceFileWatcher.
+FOUNDATION_EXPORT const unsigned char KSPDispatchSourceFileWatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSPDispatchSourceFileWatcher/PublicHeader.h>
+
+
